8ebb1ae67ada28fcab4a335cd25748822263ce5f
DenisVS <denvsim@gmail.com>
2024-08-29 12:46:10 +0200

    sibling cards header

---
+++ sibling_cards_header/deploy.sh
@@ -0,0 +1,112 @@
+#!/bin/sh
+#Install feeds tamper settings, patch for language feedback, install search system
+
+# !!!!!
+SITE_ROOT="/data/sites_php82/modular.prod"
+LIST_MODULES="list_modules.txt"
+### Main points
+# before git: git --global --add safe.directory 
+# Export current config before commit
+# chown www:www after each copy
+# chown www:www  Before export config
+# After copy config fix domain
+# Export current config before update config files
+BASEDIR=$(dirname "$0")
+
+read -p "Attention! Site URL is: ${SITE_ROOT}. Type yes" uservar
+
+FILES=$(ls "$BASEDIR"/"$LIST_MODULES" 2>/dev/null)
+if [ -n "$FILES" ]; then
+	MODULES=$(cat  "$BASEDIR"/"$LIST_MODULES") 
+	for MODULE in ${MODULES}; do
+		echo "Module: "${MODULE}
+		jexec -U www apache01 composer --working-dir="${SITE_ROOT}" require ${MODULE}
+		MODULE=$(echo ${MODULE} | grep . | awk -F \: '{print $1}' | awk -F \/ '{print $2}')
+		#MODULE=$(echo ${MODULE})
+		echo "Enable module "${MODULE}
+		jexec -U www apache01 drush -r "${SITE_ROOT}" pm:enable --yes "${MODULE}"
+	
+	done
+else
+    echo There are no modules in list to install.
+fi
+
+# patch modules
+DIR_PATCHES_M=$(ls "$BASEDIR"/"modules" 2>/dev/null)
+if [ -n "$DIR_PATCHES_M" ]; then
+	cp -Rf "$BASEDIR"/modules/* "${SITE_ROOT}"/htdocs/modules/
+	chown -R www:www "${SITE_ROOT}"/htdocs/modules/
+	
+	### modules/contrib
+	PATCHES_MODULES_CONT=$(ls "$BASEDIR"/"modules/contrib" 2>/dev/null)
+	if [ -n "$PATCHES_MODULES_CONT" ]; then
+		LIST_PATCHES_MODULES=$(ls "$BASEDIR"/"modules/contrib" 2>/dev/null)
+		#echo ASD "$LIST_PATCHES_MODULES"
+	else
+		echo There are no modules in directory \"modules/contrib\" to patch or install.
+	fi
+	### modules/custom
+	PATCHES_MODULES_CUST=$(ls "$BASEDIR"/"modules/custom" 2>/dev/null)
+	if [ -n "$PATCHES_MODULES_CUST" ]; then
+		LIST_PATCHES_MODULES="$LIST_PATCHES_MODULES
+$(ls "$BASEDIR"/"modules/custom" 2>/dev/null)"
+		#echo ASD "$LIST_PATCHES_MODULES"
+	else
+		echo There are no modules in directory \"modules/custom\" to patch or install.
+	fi	
+	LIST_PATCHES_MODULES=$(echo "$LIST_PATCHES_MODULES" | sed 's/^[ \t]*//;s/[ \t]*$//')
+	
+	echo "$LIST_PATCHES_MODULES"
+	cp -Rf "$BASEDIR"/modules/* "${SITE_ROOT}"/htdocs/modules/
+	chown -R www:www "${SITE_ROOT}"/htdocs/modules/
+	
+	for MODULE in ${LIST_PATCHES_MODULES}; do
+		echo "Module: "${MODULE}
+		jexec -U www apache01 composer --working-dir="${SITE_ROOT}" require ${MODULE}
+		MODULE=$(echo ${MODULE} | grep . | awk -F \: '{print $1}' | awk -F \/ '{print $2}')
+		#MODULE=$(echo ${MODULE})
+		echo "Enable module "${MODULE}
+		jexec -U www apache01 drush -r "${SITE_ROOT}" pm:enable --yes "${MODULE}"	
+	done
+else
+    echo There are no modules in directory \"modules\" to patch or install.
+fi
+
+# libraries
+DIR_LIBS=$(ls "$BASEDIR"/"libraries" 2>/dev/null)
+if [ -n "$DIR_LIBS" ]; then
+	cp -Rf "$BASEDIR"/libraries/* "${SITE_ROOT}"/htdocs/libraries/
+	chown -R www:www "${SITE_ROOT}"/htdocs/libraries/
+else
+    echo There are no libraries to install.
+fi
+
+# themes
+DIR_THEMES=$(ls "$BASEDIR"/"themes" 2>/dev/null)
+if [ -n "$DIR_THEMES" ]; then
+	cp -Rf "$BASEDIR"/themes/* "${SITE_ROOT}"/htdocs/themes/
+	chown -R www:www "${SITE_ROOT}"/htdocs/themes/
+else
+    echo There are no themes to install.
+fi
+
+# export current config
+jexec -U www apache01 drush -r "${SITE_ROOT}" cex
+
+# copy and import configs
+cp -Rf "$BASEDIR"/config_sync/* "${SITE_ROOT}"/config/sync/
+#/usr/local/sbin/searchreplace  '\/\/dev.mo' '\/\/mo' '/data/sites_php82/modular.prod/config/sync/*.yml'
+chown -R www:www "${SITE_ROOT}"/config/sync
+#jexec -U www apache01 drush -r "${SITE_ROOT}" cim --partial
+jexec -U www apache01 drush -r "${SITE_ROOT}" cim
+jexec -U www apache01 drush -r="${SITE_ROOT}" updatedb --yes
+jexec -U www apache01 drush -r="${SITE_ROOT}" cache:rebuild
+
+# Index search
+#jexec -U www apache01 drush -r "${SITE_ROOT}" sapi-i products
+
+
+
+
+
+
---
+++ sibling_cards_header/modules/custom/sibling_menu_block/src/Plugin/Block/SiblingMenuBlock.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace Drupal\sibling_menu_block\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+
+
+//
+/**
+ * Provides a 'Simple message' Block.
+ *
+ * @Block(
+ *   id = "sibling_menu_block",
+ *   admin_label = @Translation("Sibling menu block"),
+ *   category = @Translation("custom block example"),
+ * )
+ */
+class SiblingMenuBlock extends BlockBase
+{
+  /**
+   * {@inheritdoc}
+   * sort multidimension array
+   */
+  function cmp($a, $b)
+  {
+    return strcmp($a['weight'], $b['weight']);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build()
+  {
+    $depth = 3;
+
+    /** @var \Drupal\sibling_menu_block\Plugin\Block\SiblingMenuBlock $language  current language */
+    $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
+    /** @var \Drupal\sibling_menu_block\Plugin\Block\SiblingMenuBlock $defaultLanguage  default language */
+    $defaultLanguage = \Drupal::languageManager()->getDefaultLanguage()->getId();
+    $aliasPrefix = "/" . $language;
+    if ($language == $defaultLanguage) {
+      $aliasPrefix = '';
+    }
+
+
+    //get current tid
+    $route = \Drupal::routeMatch();
+
+    /** @var \Drupal\sibling_menu_block\Plugin\Block\SiblingMenuBlock $current_tid  id of term  where we are now */
+    $current_tid = $route->getRawParameter('taxonomy_term');
+
+
+    // get name from  tid
+    /** @var \Drupal\sibling_menu_block\Plugin\Block\SiblingMenuBlock $current_term  taxonomy therm where we are now */
+    $current_term = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->load($current_tid);
+    $current_term = $current_term->hasTranslation($language) ? $current_term->getTranslation($language) : $current_term;
+    /** @var \Drupal\sibling_menu_block\Plugin\Block\SiblingMenuBlock $current_name  name or page where we are now */
+    $current_name = $current_term->getName();
+
+    $root_parents = ["11", "69" ];
+  
+    // Here we get all parent terms
+    $allParents = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadAllParents($current_term->id());
+    $allParKeys = array_keys((array) $allParents);
+    foreach ($allParKeys as $current_allParents_key) {
+      //Now we determine, if tid exist in array. If so, this tid will be in the next function.
+      if (in_array(strval($current_allParents_key), $root_parents, true)) {
+        $root_tid = $current_allParents_key;
+      }
+    }
+
+    // Load the taxonomy tree using values.
+    /** @var \Drupal\sibling_menu_block\Plugin\Block\SiblingMenuBlock $tree  taxonomy therm tree as an object */
+    $tree = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree(
+      'products', // The taxonomy term vocabulary machine name.
+      $root_tid,                 // The "tid" of parent using "0" to get all.
+      NULL,                 // Get all levels.
+      TRUE               // Get little load of taxonomy term entity.
+    );
+    /** @var \Drupal\sibling_menu_block\Plugin\Block\SiblingMenuBlock $wholeMenuData  Formed by us the whole vocabulary with deep, determined loadTree parameters */
+    $wholeMenuData = [];
+
+
+
+
+
+
+
+    foreach ($tree as $key => $term) {
+
+      //$term = $term->getTranslation($language);
+      $term = $term->hasTranslation($language) ? $term->getTranslation($language) : $term;
+      $wholeMenuData[$key]['id'] = $term->id();
+      $wholeMenuData[$key]['name'] = $term->getName();
+      $wholeMenuData[$key]['description'] = $term->getDescription();
+      $wholeMenuData[$key]['weight'] = $term->getWeight();
+
+      // determine parent of term we processing in the cycle
+      $parent = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadParents($term->id());
+      $parent_key = array_keys($parent);
+      $wholeMenuData[$key]['parent'] = reset($parent_key);
+
+
+
+      // if current id = processig term id, then let $current_processing_parent will parent of it.
+      if ($term->id() == $current_tid) {
+        /** @var \Drupal\sibling_menu_block\Plugin\Block\SiblingMenuBlock $current_processing_parent parent of term we processing in the cycle */
+        $current_processing_parent = $wholeMenuData[$key]['parent'];
+      }
+
+      // Get depth of processing taxonomy term
+      /** @var \Drupal\sibling_menu_block\Plugin\Block\SiblingMenuBlock  $parents array of parents */
+      $parents = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadAllParents($term->id());
+      $wholeMenuData[$key]['depth'] = count($parents);
+
+      //get aliases
+      $aliasManager = \Drupal::service('path_alias.manager');
+
+
+      $wholeMenuData[$key]['uri'] = $aliasPrefix . $aliasManager->getAliasByPath('/taxonomy/term/' . $wholeMenuData[$key]['id']);
+
+
+      // get another fields
+      foreach ($term->getFields(true) as $field) {
+
+        //get image url
+        if ($field->getName() == 'field_products_category_picture') {
+          if (isset($field->getValue()[0]['target_id'])) {
+            $fid = $field->getValue()[0]['target_id']; // File ID, 
+            // Note the [0] in the array. We assume there is always going to be 1 image here. 
+            // If your field allows more than 1 image, you will need to loop through each $field->getValue()
+            $file = \Drupal\file\Entity\File::load($fid); // load File entity object
+            $file_url = \Drupal::service('file_url_generator')->generateAbsoluteString($file->getFileUri()); // get the URL (D10).
+            $wholeMenuData[$key]['image'] = $file_url;
+
+          }
+        }
+
+      }
+    }
+    // is it a top level of settings terms?
+    foreach ($wholeMenuData as $key => $field) {
+      if ($field['depth'] < $depth) {
+        //dpm($field['id'] . ' is not my sibling and not me!');
+        unset($wholeMenuData[$key]);
+      }
+
+      if ((isset($current_processing_parent) and $field['parent'] == $current_processing_parent) or $field['parent'] == $current_tid) {
+
+      } else {
+        unset($wholeMenuData[$key]);
+
+      }
+    }
+
+    // If it's me, then exclude me!
+    foreach ($wholeMenuData as $key => $field) {
+      if ($field['id'] == $current_tid) {
+        //dpm($field['id'] . ' is not my sibling and not me!');
+        unset($wholeMenuData[$key]);
+      }
+    }
+
+    usort($wholeMenuData, array('Drupal\sibling_menu_block\Plugin\Block\SiblingMenuBlock', 'cmp'));
+    $output = '<div class="wrapper_siblings">';
+
+    foreach ($wholeMenuData as $field) {
+
+      $output .= '<div class="wrapper_sibling"><div class="sibling-img">
+      <a href="' . $field['uri'] . '"><img src="' . $field['image'] . '" height="150" width="150"></a>
+      </div>
+      <div class="sibliing-info"><div class="sibling-text">
+        <H2><a href="' . $field['uri'] . '">' . $field['name'] . '</a></H2>
+        <p>' . $field['description'] . '</p>
+        </div>
+        </div>
+        </div>';
+    }
+    $output .= '<div class="siblings-header-container"><h1  class="siblings-header">'.$current_name.'</h1></div></div>';
+
+    return [
+      '#title' => $current_name,
+      '#markup' => $this->t($output),
+      '#attached' => [
+        'library' => [
+          'sibling_menu_block/sibling-cards',
+        ],
+      ],
+    ];
+  }
+
+  public function getCacheMaxAge()
+  {
+    return 0;
+  }
+
+}
+
+
