55e0b07aaf86bd7d827bcf6b018f03b599cd3770
DenisVS <denvsim@gmail.com>
2024-09-10 10:31:24 +0200

    portable scripts

--- sibling_cards_header/deploy.sh
+++ sibling_cards_header/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 #Install feeds tamper settings, patch for language feedback, install search system
-
-# !!!!!
-SITE_ROOT="/data/sites_php82/modular.dev"
+SITE_TYPE="prod" # If you have a different versions with detail in names, you can assign them here
+SITE_ROOT="/data/sites/php82/modular.$SITE_TYPE" # How looks the Site root from web server.
+SITE_DIR="/zs2T1/sites/php82/modular.$SITE_TYPE" # Site directory from your POV
 LIST_MODULES="list_modules.txt"
+WEBSERVER_CMD_PREFIX="jexec -U www apache02 " # If your commands on web server work in jail or another environment, set prefix here.
+
 ### Main points
 # before git: git --global --add safe.directory 
 # Export current config before commit
@@ -12,19 +14,29 @@ LIST_MODULES="list_modules.txt"
 # After copy config fix domain
 # Export current config before update config files
 BASEDIR=$(dirname "$0")
+echo "Attention!"
+echo "Site root on web server is: ${SITE_ROOT}"
+echo "Site directory from your POV, is: ${SITE_DIR}"
+read -p "Type \"yes\" if it's correct: " uservar
 
-read -p "Attention! Site URL is: ${SITE_ROOT}. Type yes" uservar
+uservar=`echo $uservar | grep -i yes`
+if [ -z "$uservar" ]; then
+	echo "Invalid parameter. STOP!"
+	exit
+fi
+
+
 
 FILES=$(ls "$BASEDIR"/"$LIST_MODULES" 2>/dev/null)
 if [ -n "$FILES" ]; then
 	MODULES=$(cat  "$BASEDIR"/"$LIST_MODULES") 
 	for MODULE in ${MODULES}; do
 		echo "Module: "${MODULE}
-		jexec -U www apache01 composer --working-dir="${SITE_ROOT}" require ${MODULE}
+		${WEBSERVER_CMD_PREFIX}composer --working-dir="${SITE_ROOT}" require ${MODULE}
 		MODULE=$(echo ${MODULE} | grep . | awk -F \: '{print $1}' | awk -F \/ '{print $2}')
 		#MODULE=$(echo ${MODULE})
 		echo "Enable module "${MODULE}
-		jexec -U www apache01 drush -r "${SITE_ROOT}" pm:enable --yes "${MODULE}"
+		${WEBSERVER_CMD_PREFIX}drush -r "${SITE_ROOT}" pm:enable --yes "${MODULE}"
 	
 	done
 else
@@ -34,8 +46,8 @@ fi
 # patch modules
 DIR_PATCHES_M=$(ls "$BASEDIR"/"modules" 2>/dev/null)
 if [ -n "$DIR_PATCHES_M" ]; then
-	cp -Rf "$BASEDIR"/modules/* "${SITE_ROOT}"/htdocs/modules/
-	chown -R www:www "${SITE_ROOT}"/htdocs/modules/
+	cp -Rf "$BASEDIR"/modules/* "${SITE_DIR}"/htdocs/modules/
+	chown -R www:www "${SITE_DIR}"/htdocs/modules/
 	
 	### modules/contrib
 	PATCHES_MODULES_CONT=$(ls "$BASEDIR"/"modules/contrib" 2>/dev/null)
@@ -59,17 +71,17 @@ $(ls "$BASEDIR"/"modules/custom" 2>/dev/null)"
 	LIST_PATCHES_MODULES=$(echo "$LIST_PATCHES_MODULES" | sed 's/^[ \t]*//;s/[ \t]*$//')
 	
 	echo "$LIST_PATCHES_MODULES"
-	cp -Rf "$BASEDIR"/modules/* "${SITE_ROOT}"/htdocs/modules/
-	chown -R www:www "${SITE_ROOT}"/htdocs/modules/
+	cp -Rf "$BASEDIR"/modules/* "${SITE_DIR}"/htdocs/modules/
+	chown -R www:www "${SITE_DIR}"/htdocs/modules/
 	
 	for MODULE in ${LIST_PATCHES_MODULES}; do
 		echo "Module: "${MODULE}
 		if [ "$CONTRIB_MODULES" = 1 ]; then
-			jexec -U www apache01 composer --working-dir="${SITE_ROOT}" require ${MODULE}
+			${WEBSERVER_CMD_PREFIX}composer --working-dir="${SITE_ROOT}" require ${MODULE}
 		fi
 		MODULE=$(echo ${MODULE} | grep . | awk -F \: '{print $1}' | awk -F \/ '{print $2}')
 		echo "Enable module "${MODULE}
-		jexec -U www apache01 drush -r "${SITE_ROOT}" pm:enable --yes "${MODULE}"	
+		${WEBSERVER_CMD_PREFIX}drush -r "${SITE_ROOT}" pm:enable --yes "${MODULE}"	
 	done
 else
     echo There are no modules in directory \"modules\" to patch or install.
@@ -78,8 +90,8 @@ fi
 # libraries
 DIR_LIBS=$(ls "$BASEDIR"/"libraries" 2>/dev/null)
 if [ -n "$DIR_LIBS" ]; then
-	cp -Rf "$BASEDIR"/libraries/* "${SITE_ROOT}"/htdocs/libraries/
-	chown -R www:www "${SITE_ROOT}"/htdocs/libraries/
+	cp -Rf "$BASEDIR"/libraries/* "${SITE_DIR}"/htdocs/libraries/
+	chown -R www:www "${SITE_DIR}"/htdocs/libraries/
 else
     echo There are no libraries to install.
 fi
@@ -87,26 +99,26 @@ fi
 # themes
 DIR_THEMES=$(ls "$BASEDIR"/"themes" 2>/dev/null)
 if [ -n "$DIR_THEMES" ]; then
-	cp -Rf "$BASEDIR"/themes/* "${SITE_ROOT}"/htdocs/themes/
-	chown -R www:www "${SITE_ROOT}"/htdocs/themes/
+	cp -Rf "$BASEDIR"/themes/* "${SITE_DIR}"/htdocs/themes/
+	chown -R www:www "${SITE_DIR}"/htdocs/themes/
 else
     echo There are no themes to install.
 fi
 
 # export current config
-jexec -U www apache01 drush -r "${SITE_ROOT}" cex
+${WEBSERVER_CMD_PREFIX}drush -r "${SITE_ROOT}" cex
 
 # copy and import configs
-cp -Rf "$BASEDIR"/config_sync/* "${SITE_ROOT}"/config/sync/
+cp -Rf "$BASEDIR"/config_sync/* "${SITE_DIR}"/config/sync/
 #/usr/local/sbin/searchreplace  '\/\/dev.mo' '\/\/mo' '/data/sites_php82/modular.prod/config/sync/*.yml'
-chown -R www:www "${SITE_ROOT}"/config/sync
-#jexec -U www apache01 drush -r "${SITE_ROOT}" cim --partial
-jexec -U www apache01 drush -r "${SITE_ROOT}" cim
-jexec -U www apache01 drush -r="${SITE_ROOT}" updatedb --yes
-jexec -U www apache01 drush -r="${SITE_ROOT}" cache:rebuild
+chown -R www:www "${SITE_DIR}"/config/sync
+#${WEBSERVER_CMD_PREFIX}drush -r "${SITE_ROOT}" cim --partial
+${WEBSERVER_CMD_PREFIX}drush -r "${SITE_ROOT}" cim
+${WEBSERVER_CMD_PREFIX}drush -r="${SITE_ROOT}" updatedb --yes
+${WEBSERVER_CMD_PREFIX}drush -r="${SITE_ROOT}" cache:rebuild
 
 # Index search
-#jexec -U www apache01 drush -r "${SITE_ROOT}" sapi-i products
+#${WEBSERVER_CMD_PREFIX}drush -r "${SITE_ROOT}" sapi-i products
 
 
 
